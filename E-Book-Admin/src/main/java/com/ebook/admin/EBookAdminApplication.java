1f5a1b359bdc5eb4376808b41ad80b89a27da8a9
bgoddu <bgoddu@BGODDU-L-941.evoketechnologies.com>
2020-11-28 17:34:40 +0530

    initial commit

---
+++ E-Book-Admin/src/main/java/com/ebook/admin/EBookAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ebook.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBookAdminApplication.class, args);
+	}
+
+}
---
+++ E-Book-Admin/src/test/java/com/ebook/admin/EBookAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebook.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBookAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e80e5eb4c5eaec61ce35cdd85745c7a9234c6d53
bgoddu <bgoddu@BGODDU-L-941.evoketechnologies.com>
2020-12-02 10:41:15 +0530

    delete flow

--- E-Book-Admin/src/main/java/com/ebook/admin/controller/BookController.java
+++ E-Book-Admin/src/main/java/com/ebook/admin/controller/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.convert.ReadingConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,6 +59,12 @@ public class BookController {
 	@PostMapping("/update")
 	public String updateBookPost(@ModelAttribute("book") Book book) {
 		bookService.save(book);
+		return "redirect:bookList";
+	}
+	
+	@GetMapping("/delete")
+	public String deleteBook(@RequestParam Long id) {
+		bookService.removeOne(id);
 		return "redirect:bookList";
 	}
 	
